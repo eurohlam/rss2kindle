0fda0270066d0bd6782b4387843f7f09535afdf9
eurohlam <eurohlam@gmail.com>
2016-10-27 17:58:42 +1300

    Add files via upload

--- kindle/kindlegen.sh
+++ kindle/kindlegen.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+PATH=$1
+KINDLE_HOME="/mnt/Data/Projects/Integration/RSS2Kindle/kindle"
+
+if [ -z ${PATH} ]; then
+    echo "Empty path";
+    exit -2;
+else 
+    if [ -d ${PATH} ]; then
+	echo "Scan and convert *.htm in ${PATH}";
+	for file in ${PATH}/*.htm
+	do
+	    echo "Convert file ${file}";
+	    ${KINDLE_HOME}/kindlegen ${file} -c1 -c0 -locale ru -verbose >conversion.out;
+	done
+    else
+	echo "Convert ${PATH}";
+	${KINDLE_HOME}/kindlegen ${PATH} -c1 -c0 -locale ru -verbose >conversion.out;
+    fi
+fi
+
+echo "Conversion is done"
+
 
