706e372e67f66e50057ca23cfe720578159ed880
eurohlam <eurohlam@gmail.com>
2016-10-27 17:56:59 +1300

    Add files via upload

--- test/java/org/roag/camel/CamelContextTest.java
+++ test/java/org/roag/camel/CamelContextTest.java
@@ -1,1 +1,24 @@
+package org.roag.camel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by eurohlam on 06.10.16.
+ */
+public class CamelContextTest
+{
+    final public static Logger logger= LoggerFactory.getLogger(ServiceLocator.class);
+
+    @Test(groups = {"checking"})
+    public void contextLoaderTest()
+    {
+        logger.info("Run spring context testing");
+        assertTrue("Bean MongoHelper has not loaded from context", ServiceLocator.getContext().containsBeanDefinition("mongoHelper"));
+    }
+
+
+}
