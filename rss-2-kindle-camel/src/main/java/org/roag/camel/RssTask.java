6d3cbf3c1e6fb2363a3f31b044cf498780d1c25b
eurohlam <eurohlam@gmail.com>
2017-06-30 00:28:14 +1200

    rss polling redesing

---
+++ rss-2-kindle-camel/src/main/java/org/roag/camel/RssTask.java
@@ -0,0 +1,60 @@
+package org.roag.camel;
+
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.feed.synd.SyndFeedImpl;
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.dataformat.rss.RssConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by eurohlam on 29.06.17.
+ */
+public class RssTask implements Callable<Map<String, String>>
+{
+    final private static Logger logger = LoggerFactory.getLogger(RssTask.class);
+
+    final private ConsumerTemplate consumer;
+    final private String rssURI;
+    final private String path;
+    final private String fileName;
+
+    public RssTask(ConsumerTemplate consumer, String rssURI, String path, String fileName)
+    {
+        this.consumer = consumer;
+        this.rssURI = rssURI;
+        this.path = path;
+        this.fileName = fileName;
+    }
+
+    @Override
+    public Map<String, String> call() throws Exception
+    {
+        Map<String, String> map = new Hashtable<>(1);
+        logger.debug("Started polling {}", rssURI);
+        SyndFeed feed = consumer.receiveBody(rssURI, SyndFeedImpl.class);
+        logger.debug("Finished polling {}.\nTitle is {}.\nDescription is {}", rssURI, feed.getTitle(), feed.getDescription());
+
+        File folder=new File(path);
+        if (!folder.exists())
+            folder.mkdirs();
+        if (folder.exists() && folder.isDirectory())
+        {
+            String file=folder.getPath() + "/" + fileName;
+            OutputStream out = new FileOutputStream(file);
+            out.write(RssConverter.feedToXml(feed).getBytes());
+            out.close();
+            logger.debug("Feed {} marshaled into file {}", rssURI, file);
+            map.put(rssURI, file);
+        }
+        return map;
+    }
+}
