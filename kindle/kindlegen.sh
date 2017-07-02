4feefff46d4e31aabd92aeb2aecc0bc0c110001a
eurohlam <eurohlam@gmail.com>
2017-07-02 21:11:46 +1200

    camel flow redesign

--- kindle/kindlegen.sh
+++ kindle/kindlegen.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
 PATH=$1
-#KIDNLE_HOME has to be defined
-KINDLE_HOME=""
-
-if [ -z ${KINDLE_HOME} ]; then
-    echo "Empty KINDLE_HOME";
-    exit -2;
-fi
+KINDLE_HOME="/mnt/Data/Projects/Integration/RSS2Kindle/kindle"
 
 if [ -z ${PATH} ]; then
-    echo "Empty path";
+    echo "Empty path">>logs/kindle.log;
     exit -2;
 else 
+    #check if $PATH is a directory
     if [ -d ${PATH} ]; then
-	echo "Scan and convert *.htm in ${PATH}";
+	echo "Scan and convert *.htm in the directory ${PATH}">>logs/kindle.log ;
 	for file in ${PATH}/*.htm
 	do
-	    echo "Convert file ${file}";
-	    ${KINDLE_HOME}/kindlegen ${file} -c1 -c0 -locale ru -verbose >conversion.out;
+	    echo "Convert file ${file}">>logs/kindle.log;
+	    ${KINDLE_HOME}/kindlegen ${file} -c1 -c0 -locale ru -verbose >>logs/conversion.out;
 	done
     else
-	echo "Convert ${PATH}";
-	${KINDLE_HOME}/kindlegen ${PATH} -c1 -c0 -locale ru -verbose >conversion.out;
+	echo "Convert file ${PATH}" >>logs/kindle.log;
+	${KINDLE_HOME}/kindlegen ${PATH} -c1 -c0 -locale ru -verbose >>logs/conversion.out;
     fi
 fi
 
-echo "Conversion is done"
+echo "Conversion is done" >>logs/kindle.log
 
 
--- rss-2-kindle-camel/src/main/java/org/roag/camel/MainApp.java
+++ rss-2-kindle-camel/src/main/java/org/roag/camel/MainApp.java
@@ -24,7 +24,7 @@ public class MainApp
                     {
                         from("timer://runOnce?repeatCount=1&delay=5000").
                                 log(LoggingLevel.INFO, "org.roag.camel.Main", "Run RSS polling for all").
-                                bean("RSS2XMLBuilder", "runRssPollingForAllSubscribers");
+                                bean("rss2XmlHandler", "runRssPollingForAllSubscribers");
                     }
                 }
         );
