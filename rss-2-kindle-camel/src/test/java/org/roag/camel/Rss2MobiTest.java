f962529b0e38c5337dba1c7442be509f68c2dd59
eurohlam <eurohlam@gmail.com>
2017-08-30 00:17:00 +1200

    update camel unit test

--- rss-2-kindle-camel/src/test/java/org/roag/camel/Rss2MobiTest.java
+++ rss-2-kindle-camel/src/test/java/org/roag/camel/Rss2MobiTest.java
@@ -1,14 +1,13 @@
 package org.roag.camel;
 
-import org.apache.camel.RoutesBuilder;
-import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.PropertyInject;
 import org.apache.camel.testng.CamelSpringTestSupport;
 import org.junit.Assert;
-import org.junit.Test;
-import org.roag.ds.SubscriberRepository;
-import org.roag.model.Subscriber;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.FileSystemUtils;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -24,15 +23,18 @@ import java.io.FileOutputStream;
 public class Rss2MobiTest extends CamelSpringTestSupport
 {
 
+
     private static final String rss_file = "src/test/resources/testrss.xml";
-    private static final String text_file = "src/test/resources/test_output.html";
+    private static final String output_file = "test_output";
+
     private static final String text_xslt = "src/main/resources/xslt/rss2html.xsl";
-    private static final String toc_file = "src/test/resources/toc.html";
     private static final String toc_xslt = "src/main/resources/xslt/toc.xsl";
-    private static final String ncx_file = "src/test/resources/toc.ncx";
     private static final String ncx_xslt = "src/main/resources/xslt/ncx.xsl";
-    private static final String opf_file = "src/test/resources/test_output.opf";
     private static final String opf_xslt = "src/main/resources/xslt/opf.xsl";
+
+    @PropertyInject("storage.path.mobi")
+    private String pathMobi;
+
 
     @Override
     protected AbstractApplicationContext createApplicationContext()
@@ -64,44 +66,54 @@ public class Rss2MobiTest extends CamelSpringTestSupport
         context.stop();
     }
 
+    @BeforeMethod
+    @Override
+    public void setUp() throws Exception
+    {
+        File file = new File("test/data");
+        if(!FileSystemUtils.deleteRecursively(file)) {
+            System.out.println("Problem occurs when deleting the directory : test/data" );
+        }
+        super.setUp();
+    }
 
-    @Test
+
+
+    @Test(groups = {"transformation"})
     public void rss2htmlTransformationTest()
     {
         try {
+            File dir=new File(pathMobi);
+            if (!dir.exists())
+                dir.mkdirs();
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer(new StreamSource(new FileInputStream(text_xslt)));
-            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(text_file)));
-            Assert.assertTrue("Transformation for text file does not work", new File(text_file).exists());
+            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(pathMobi+ output_file+".htm")));
+            Assert.assertTrue("Transformation for text file does not work", new File(pathMobi+ output_file+".htm").exists());
 
             transformer = factory.newTransformer(new StreamSource(new FileInputStream(toc_xslt)));
-            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(toc_file)));
-            Assert.assertTrue("Transformation for toc file does not work", new File(toc_file).exists());
+            transformer.setParameter("output_file", "test_output");
+            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(pathMobi+output_file+"_toc.htm")));
+            Assert.assertTrue("Transformation for toc file does not work", new File(pathMobi+output_file+"_toc.htm").exists());
 
             transformer = factory.newTransformer(new StreamSource(new FileInputStream(ncx_xslt)));
-            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(ncx_file)));
-            Assert.assertTrue("Transformation for ncx file does not work", new File(ncx_file).exists());
+            transformer.setParameter("output_file", "test_output");
+            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(pathMobi+output_file+".ncx")));
+            Assert.assertTrue("Transformation for ncx file does not work", new File(pathMobi+output_file+".ncx").exists());
 
             transformer = factory.newTransformer(new StreamSource(new FileInputStream(opf_xslt)));
-            transformer.setParameter("output_file", "test_output.html");
-            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(opf_file)));
-            Assert.assertTrue("Transformation for opf file does not work", new File(opf_file).exists());
+            transformer.setParameter("output_file", "test_output");
+            transformer.transform(new StreamSource(rss_file), new StreamResult(new FileOutputStream(pathMobi+output_file+".opf")));
+            Assert.assertTrue("Transformation for opf file does not work", new File(pathMobi+output_file+".opf").exists());
+
+            template.sendBodyAndHeader("seda:kindlegen", null, "CamelFileName", output_file+".opf");
+            Thread.sleep(10000);
+            Assert.assertTrue("Transformation for mobi file does not work", new File(pathMobi+output_file+".mobi").exists());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    @Override
-    protected RoutesBuilder createRouteBuilder() throws Exception
-    {
-        return new RouteBuilder()
-        {
-            @Override
-            public void configure() throws Exception
-            {
-                from("direct:xslt").to("${}");
-            }
-        };
-    }
 }
