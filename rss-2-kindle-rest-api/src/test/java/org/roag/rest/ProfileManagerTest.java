b036105242c4221f0bd8e8c7d5e902578f98a361
Roman Agafonov <romana@datacom.co.nz>
2017-09-05 17:06:49 +1200

    jersey unit test

--- rss-2-kindle-rest-api/src/test/java/org/roag/rest/ProfileManagerTest.java
+++ rss-2-kindle-rest-api/src/test/java/org/roag/rest/ProfileManagerTest.java
@@ -2,6 +2,11 @@ package org.roag.rest;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTestNg;
+import org.roag.ds.SubscriberRepository;
+import org.roag.model.Subscriber;
+import org.roag.service.SubscriberFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.Application;
@@ -15,6 +20,20 @@ import static org.junit.Assert.*;
  */
 public class ProfileManagerTest extends JerseyTestNg.ContainerPerClassTest
 {
+
+    @Autowired
+    private SubscriberRepository subscriberRepository;
+    @Autowired
+    private Subscriber testSubscriber;
+    @Autowired
+    private SubscriberFactory subscriberFactory = new SubscriberFactory();
+
+    @BeforeClass
+    @Override
+    public void setUp() throws Exception {
+        subscriberRepository.addSubscriber(testSubscriber);
+    }
+
     @Override
     protected Application configure()
     {
@@ -24,7 +43,7 @@ public class ProfileManagerTest extends JerseyTestNg.ContainerPerClassTest
     @Test(groups = {"profile"})
     public void getAllSubscriptionTest()
     {
-        final Response response = target("profile/fbroman@mail.ru/subscriptions").request().accept(MediaType.APPLICATION_JSON_TYPE).get();
+        final Response response = target("profile/test@mail.com/subscriptions").request().accept(MediaType.APPLICATION_JSON_TYPE).get();
 
         assertEquals(response.getStatus(), 200);
     }
