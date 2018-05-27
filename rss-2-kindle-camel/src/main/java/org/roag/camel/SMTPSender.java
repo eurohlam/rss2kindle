6a29d92d4da26dd3c500cc72e341e867b0d48150
eurohlam <eurohlam@gmail.com>
2018-05-27 21:19:55 +1200

    Updated SMTPSender. Removed unittest for SMTPSender due to it does not work properly

--- rss-2-kindle-camel/src/main/java/org/roag/camel/SMTPSender.java
+++ rss-2-kindle-camel/src/main/java/org/roag/camel/SMTPSender.java
@@ -1,13 +1,12 @@
 package org.roag.camel;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.CamelExecutionException;
-import org.apache.camel.ProducerTemplate;
+import org.apache.camel.*;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,43 +17,50 @@ import java.util.Map;
  * Created by eurohlam on 23/05/18.
  */
 @Service
-public class SMTPSender {
+public class SMTPSender extends RouteBuilder {
 
     final private static Logger logger = LoggerFactory.getLogger(SMTPSender.class);
-
-    private CamelContext context;
 
     private String uri;
     private String from;
 
     @Autowired
-    public SMTPSender(@Value("${smtp.uri}") String uri, @Value("${smtp.from}") String from) throws Exception{
+    public SMTPSender(@Value("${smtp.uri}") String uri, @Value("${smtp.from}") String from,
+                      @Qualifier("mainCamelContext") CamelContext camelContext) throws Exception {
         this.uri = uri;
         this.from = from;
-        this.context = new DefaultCamelContext();
-        context.start();
+    }
+
+    @Override
+    public void configure() throws Exception {
+        from("direct:sendEmail").log("Sending email to ${header.to} with subject ${header.subject}").to(uri);
+    }
+
+    public void send(String host, String port, String to, String subject, String message) {
+        logger.info("Sending new email via smtp: {}:{}; to: {}; from: {}; subject: {}; body: {}", host, port, to, from, subject, message);
+        ProducerTemplate template = getContext().createProducerTemplate();
+        Map<String, Object> headers = new HashMap<String, Object>();
+        headers.put("to", to);
+        headers.put("from", from);
+        headers.put("subject", subject);
+        headers.put("contentType", "text/plain");
+        template.sendBodyAndHeaders("smtp://" + host + ":" + port, message, headers);
+        logger.info("Email has been sent successfully");
     }
 
     public void send(String to, String subject, String message) {
-        try {
-            logger.info("Sending new email via smtp: {}; to: {}; from: {}; subject: {}; body: {}", uri, to, from, subject, message);
-            ProducerTemplate template = context.createProducerTemplate();
-            Map<String, Object> headers = new HashMap<String, Object>();
-            headers.put("to", to);
-            headers.put("from", from);
-            headers.put("subject", subject);
-            headers.put("contentType", "text/plain");
-            template.sendBodyAndHeaders(uri, message, headers);
-            logger.info("Email has been sent successfully");
-
-//            Thread.sleep(5000); //wait for while before stopping context
-//            context.stop();
-        } catch (CamelExecutionException e) {
-            logger.error(e.getMessage(), e);
-        }
-    }
-
-    public CamelContext getContext() {
-        return context;
+        logger.info("Sending new email via smtp: {}; to: {}; from: {}; subject: {}; body: {}", uri, to, from, subject, message);
+        Endpoint endpoint = getContext().getEndpoint("direct:sendEmail");
+        Exchange exchange = endpoint.createExchange();
+        Message in = exchange.getIn();
+        in.setHeader("subject", subject);
+        in.setHeader("to", to);
+        in.setHeader("from", from);
+        in.setHeader("contentType", "text/plain");
+        in.setBody(message);
+        Producer producer = endpoint.createProducer();
+        producer.start();
+        producer.process(exchange);
+        logger.info("Email has been sent successfully");
     }
 }
