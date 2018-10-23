4a2bb6d638afd7bd3bd0ace2557575fd4adf0cb4
Roman Agafonov <romana@datacom.co.nz>
2018-10-23 16:37:10 +1300

    Fixed aside menu for Admin UI

--- rss-2-kindle-web/src/main/java/org/roag/web/AbstractController.java
+++ rss-2-kindle-web/src/main/java/org/roag/web/AbstractController.java
@@ -1,8 +1,14 @@
 package org.roag.web;
 
+import org.roag.model.Roles;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by eurohlam on 3/09/2018.
@@ -22,6 +28,16 @@ public class AbstractController {
         return userName;
     }
 
+    @ModelAttribute("roles")
+    public List<String> getRoles() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            UserDetails ud = (UserDetails) principal;
+            return ud.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        }
+        return Arrays.asList(Roles.ROLE_USER.toString());
+    }
 }
 
 
